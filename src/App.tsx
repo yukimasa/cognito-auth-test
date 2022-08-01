1814584e8209c8c67bec6e3ac036c7715253cc85
yukimasa <yyykms123@gmail.com>
2022-08-01 19:10:07 +0900

    feat: fetch user group

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 // import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
 function App() {
+  // 特定のグループに所属していないユーザーは強制ログアウトする
+  // useEffect(() => {
+  //   Auth.currentSession().then((user) => {
+  //     const { payload } = user.getIdToken();
+  //     if (
+  //       !payload ||
+  //       !payload["cognito:groups"] ||
+  //       !payload["cognito:groups"].includes("administrators")
+  //     ) {
+  //       Auth.signOut();
+  //     }
+  //   });
+  // }, []);
+
+  // Amplify SDKを使用してユーザーのグループ取得
+  Auth.currentSession().then((user) => {
+    const { payload } = user.getIdToken();
+    if (payload && payload["cognito:groups"]) {
+      console.log(payload["cognito:groups"]);
+    }
+  });
+
   return (
     <div className="App">
       <Authenticator>
